bc21dd6de36d3f933e9aa585565d83eb797fb226
Alexander Simatov <40271338+asimatov@users.noreply.github.com>
2025-08-20 17:13:36 +0100

    1480 / Java

---
+++ 1480/Java/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+class Solution {
+	public static void main(String[] args) {
+		record TestCase(int[] input, int[] expected) {}
+		
+		TestCase[] tests = {
+				new TestCase(new int[]{1,2,3,4}, new int[]{1,3,6,10}),
+				new TestCase(new int[]{1,1,1,1,1}, new int[]{1,2,3,4,5}),
+				new TestCase(new int[]{3,1,2,10,1}, new int[]{3,4,6,16,17})
+		};
+		
+		Solution s = new Solution();
+		for(TestCase t : tests) {
+			int[] result = s.runningSum(t.input());
+			assert Arrays.equals(t.expected(), result) : String.format("runningSum(%s) == %s, expected %s", t.input(), result.toString(), t.expected().toString()); 
+		}
+		
+	}
+	
+	public int[] runningSum(int[] nums) {
+		int[] result = new int[nums.length];
+		int sum = 0;
+		for(int i=0; i<nums.length; i++) {
+			sum += nums[i];
+			result[i] = sum;
+		}
+		
+		return result;
+    }
+}
