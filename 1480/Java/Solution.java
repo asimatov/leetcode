0d22939896d5e3b12bb089e77ec1dc36d2db030f
Alexander Simatov <40271338+asimatov@users.noreply.github.com>
2025-08-20 17:23:40 +0100

    1480 / Java

--- 1480/Java/Solution.java
+++ 1480/Java/Solution.java
@@ -7,13 +7,13 @@ class Solution {
 		TestCase[] tests = {
 				new TestCase(new int[]{1,2,3,4}, new int[]{1,3,6,10}),
 				new TestCase(new int[]{1,1,1,1,1}, new int[]{1,2,3,4,5}),
-				new TestCase(new int[]{3,1,2,10,1}, new int[]{3,4,6,16,17})
+				new TestCase(new int[]{3,1,2,10,1}, new int[]{3,4,6,16,1})
 		};
 		
 		Solution s = new Solution();
 		for(TestCase t : tests) {
 			int[] result = s.runningSum(t.input());
-			assert Arrays.equals(t.expected(), result) : String.format("runningSum(%s) == %s, expected %s", t.input(), result.toString(), t.expected().toString()); 
+			assert Arrays.equals(t.expected(), result) : String.format("runningSum(%s) == %s, expected %s", Arrays.toString(t.input()), Arrays.toString(result), Arrays.toString(t.expected())); 
 		}
 		
 	}
